f5f198059240fd3cc8d2f86a892e52d5fda0b4aa
sah-neves <sah_neves_@hotmail.com>
2025-07-29 17:31:31 -0300

    Add files via upload

---
+++ sabrina_nevesApp.swift
@@ -0,0 +1,17 @@
+//
+//  sabrina_nevesApp.swift
+//  sabrina neves
+//
+//  Created by Mack Aluno on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct sabrina_nevesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

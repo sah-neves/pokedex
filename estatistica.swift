7d1d95753eb93586585173db68702a4259c74a99
sah-neves <sah_neves_@hotmail.com>
2025-07-29 18:13:12 -0300

    Add files via upload
    abas

---
+++ estatistica.swift
@@ -0,0 +1,20 @@
+//
+//  estatistica.swift
+//  projetoPokedex
+//
+//  Created by Mack Aluno on 29/07/25.
+//
+
+import SwiftUI
+
+struct estatistica: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct estatistica_Previews: PreviewProvider {
+    static var previews: some View {
+        estatistica()
+    }
+}
---
+++ pokedex.swift
@@ -0,0 +1,20 @@
+//
+//  pokedex.swift
+//  projetoPokedex
+//
+//  Created by Mack Aluno on 29/07/25.
+//
+
+import SwiftUI
+
+struct pokedex: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct pokedex_Previews: PreviewProvider {
+    static var previews: some View {
+        pokedex()
+    }
+}

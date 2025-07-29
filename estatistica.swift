6b82840be4cff5364973ae5186d6aa323fbaca2d
sah-neves <sah_neves_@hotmail.com>
2025-07-29 18:28:06 -0300

    ranking

--- estatistica.swift
+++ estatistica.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct estatistica: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Button("Ranking por status"){
+                
+            }
+            Button("Capturados"){
+                
+            }
+        }
     }
 }
 

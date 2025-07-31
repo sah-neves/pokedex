21c7268b19d5652dc2d71e751e4fe70ae7c2c10b
lusioncx <arnodoriancreedassasins@gmail.com>
2025-07-31 18:46:40 -0300

    Add files via upload

---
+++ DetalhesPokemonView.swift
@@ -0,0 +1,51 @@
+//
+//  DetalhesPokemonView.swift
+//  caiopoke
+//
+//  Created by Aluno Mack on 31/07/25.
+//
+
+import SwiftUI
+
+struct DetalhesPokemonView: View {
+    let pokemon: Pokemon
+    var body: some View {
+        VStack{
+            AsyncImage(url: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(pokemon.id).png")) { image in
+                image.image
+            }
+            Text(pokemon.name)
+                .bold()
+                
+            
+            Text("ID:\(pokemon.id)")
+                .bold()
+                
+            Text("Tipos")
+                .font(.headline)
+            Text("HP")
+            
+            Text("HP")
+
+            Text("HP")
+
+            Text("HP")
+
+            Text("HP")
+
+            
+            
+
+            Spacer()
+            
+        }
+        .padding()
+    }
+}
+
+struct DetalhesPokemonView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+

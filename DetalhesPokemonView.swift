e4f03371dcefc9b42277b41f004e389b0d555481
lusioncx <arnodoriancreedassasins@gmail.com>
2025-08-01 17:41:14 -0300

    alterações dos nomes

--- DetalhesPokemonView.swift
+++ DetalhesPokemonView.swift
@@ -1,54 +1,51 @@
 //
-//  SwiftUIView.swift
-//  Pokedex
+//  DetalhesPokemonView.swift
+//  caiopoke
 //
-//  Created by Sofia Melo on 01/08/25.
+//  Created by Aluno Mack on 31/07/25.
 //
 
 import SwiftUI
 
 struct DetalhesPokemonView: View {
     let pokemon: Pokemon
-
     var body: some View {
-        VStack {
+        VStack{
             Text("Detalhes:")
                 .font(.system(size: 20))
-            
             // Imagem do Pokémon
             AsyncImage(url: URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(pokemon.id).png")) { image in
                 image
                     .image?.resizable()
                     .aspectRatio(contentMode: .fit)
-                    .frame(width: UIScreen.main.bounds.width * 1)
+                    .frame(width: UIScreen.main.bounds.width * 1.2)
             }
-
             // Nome do Pokémon
-            Text(pokemon.name.capitalized)
+            Text(pokemon.name)
                 .bold()
-                .font(.system(size: 30))
-                .padding(.top, -10)
-                .padding(.bottom, 10)
-
+                .foregroundColor(.black)
+                .padding(.top , -30)
+                
             // ID do Pokémon
             Text("ID:\(pokemon.id)")
                 .bold()
-
-            Text("Tipos")
-                .font(.headline)
-
-            // Placeholder de atributos
+                .foregroundColor(.gray)
+        
+            
             List{
-                Text("HP")
-                Text("HP")
-                Text("HP")
-                Text("HP")
-                Text("HP")
-                    
+                // Placeholder de atributos
+                Text("HP:\(pokemon.HP)")
+                Text("ATK:\(pokemon.ATK)")
+                Text("DEF:\(pokemon.DEF)")
+                Text("VEL:\(pokemon.Vel)")
+                Text("Special:\(pokemon.Special)")
+                
+                Text("TOTAL:\(pokemon.Total)")
             }
             .scrollContentBackground(.hidden) // Oculta fundo padrão do sistema
             .background(Color.blue.opacity(0.1))
-
+            .cornerRadius(12)
+            
         }
         .padding()
     }
